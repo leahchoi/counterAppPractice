ef2d22ac9ea9f1614646162ab3751a5c0e1fee25
leahchoi <soora913@gmail.com>
2018-06-07 16:55:03 -0700

    nothing changed

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
 document.querySelector(".incrementBox").addEventListener("click", boxClicked)
 var boxValue = document.querySelector(".value");
 var counter = 0;
-function boxClicked(){
+function displayValue() {
+    boxValue.textContent = counter
+}
+function boxClicked() {
     counter++
     displayValue();
 }
-function displayValue(){
-    boxValue.textContent = counter
+function hoveredBox() {
+    clearInterval();
 }
